46e76669fc46ce0b3c49cdebd143c520b8211d58
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-04-09 17:45:00 +0530

    Created authoption methos for authenticating

---
+++ src/lib/auth.ts
@@ -0,0 +1,59 @@
+import { UpstashRedisAdapter } from "@next-auth/upstash-redis-adapter";
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google"
+import { db } from "./db";
+
+function getGoogleCredentials() {
+    const clientId = process.env.GOOGLE_CLIENT_ID
+    const clientSecret = process.env.GOOGLE_CLIENT_SECRET
+    if (!clientId || clientId.length === 0) {
+        throw new Error('Misiing Client Id')
+    }
+    if (!clientSecret || clientSecret.length === 0) {
+        throw new Error('Misiing Client Secret')
+    }
+    return { clientId, clientSecret }
+}
+
+export const authOptions: NextAuthOptions = {
+    adapter: UpstashRedisAdapter(db),
+    session: {
+        strategy: 'jwt'
+    },
+    pages: {
+        signIn: '/login',
+    },
+    providers: [
+        GoogleProvider({
+            clientId: getGoogleCredentials().clientId,
+            clientSecret: getGoogleCredentials().clientSecret,
+        }),
+    ],
+    callbacks: {
+        async jwt({ token, user }) {
+            const dbUser = (await db.get(`user:${token.id}`)) as User | null
+            if (!dbUser) {
+                token.id = user!.id
+                return token
+            }
+            return {
+                id: dbUser.id,
+                name: dbUser.name,
+                email: dbUser.email,
+                picture: dbUser.image
+            }
+        },
+        async session({ session, token }) {
+            if (token) {
+                session.user.id = token.id
+                session.user.name = token.name
+                session.user.email = token.email
+                session.user.image = token.picture
+            }
+            return session
+        },
+        redirect() {
+            return '/dashboard'
+        }
+    }
+}
