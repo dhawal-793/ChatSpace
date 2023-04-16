2f0fa4aa262dfcbe77e32fe3e8bf901516b80a88
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-04-16 10:29:12 +0530

    Added add route under dashboard route

---
+++ src/app/(dashboard)/dashboard/add/page.tsx
@@ -0,0 +1,11 @@
+import AddFriend from "@/components/ui/AddFriend"
+
+
+const Page = () => {
+    return <main>
+        <h1 className="font-bold text-5xl mb-8 ">Add a Friend</h1>
+        <AddFriend />
+    </main>
+}
+
+export default Page
