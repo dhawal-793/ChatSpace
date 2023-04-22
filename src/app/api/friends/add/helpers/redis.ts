9fdc999ac012b1ce08403fa3243ca1c9d6f87fb9
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-04-22 20:24:53 +0530

    created a simple helper function for redis

---
+++ src/app/api/friends/add/helpers/redis.ts
@@ -0,0 +1,22 @@
+const UpstashRedisRestUrl = process.env.UPSTASH_REDIS_REST_URL;
+const authToken = process.env.UPSTASH_REDIS_REST_TOKEN;
+
+
+type Command = 'zrange' | 'sismember' | 'get' | 'smembers'
+
+export async function fetchRedis(command: Command, ...args: (string | number)[]) {
+    const commandUrl = `${UpstashRedisRestUrl}/${command}/${args.join('/')}`
+
+    const response = await fetch(commandUrl, {
+        headers: {
+            Authorization: `Bearer ${authToken}`
+        },
+        cache: "no-store"
+    })
+
+    if (!response.ok) {
+        throw new Error(`Error executing redis command: ${response.statusText}`)
+    }
+    const data = await response.json()
+    return data.result
+}
