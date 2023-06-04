ef78894070862bd50150acbb57702054dc4c3a78
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-06-04 11:41:24 +0530

    Created sidebaroOtions and maped over it

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,13 +1,31 @@
-import { Icons } from '@/components/ui/Icons'
+import { Icon, Icons } from '@/components/ui/Icons'
 import { authOptions } from '@/lib/auth'
 import { getServerSession } from 'next-auth'
 import Link from 'next/link'
 import { notFound } from 'next/navigation'
-import { FC, ReactNode } from 'react'
+import { ReactNode } from 'react'
 
 interface LayoutProps {
     children: ReactNode
 }
+
+interface SidebarOptions {
+    id: number
+    name: string
+    href: string
+    Icon: Icon
+
+}
+
+const sidebaroptions: SidebarOptions[] = [
+    {
+        id: 1,
+        name: 'Add Friend',
+        href: '/dashboard/add',
+        Icon: 'UserPlus'
+    }
+]
+
 
 const Layout = async ({ children }: LayoutProps) => {
     const session = await getServerSession(authOptions)
@@ -28,10 +46,21 @@ const Layout = async ({ children }: LayoutProps) => {
                         <div className='text-xs font-semibold leading-6 text-gray-600'>
                             Overview
                         </div>
+                        <ul role="list" className='-mx-2 mt-2 space-y-1'>
+                            {sidebaroptions.map((option) => {
+                                const Icon = Icons[option.Icon]
+                                return (
+                                    <li key={option.id}>
+                                        <Link href={option.href} className="text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex gap-3 rounded-md p-2 text-sm leading-6 font-semibold">
+                                            <span className='text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white'>{<Icon className='h-4 w-4' />}</span>
+                                            <span className='truncate'>{option.name}</span>
+                                        </Link>
+                                    </li>
+                                )
+                            })}
+                        </ul>
                     </li>
-                    <ul role="list" className='-mx-2 mt-2 space-y-1'>
-                        
-                    </ul>
+
                 </ul>
             </nav>
         </div>
