288e33fb38e52f454623a03506ec170a3d957c3f
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-06-18 23:22:50 +0530

    Added FriendRequestSidebarOptions to layout

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,3 +1,5 @@
+import { fetchRedis } from '@/app/api/friends/add/helpers/redis'
+import FriendRequestSidebarOptions from '@/components/FriendRequestSidebarOptions'
 import SignOutButton from '@/components/SignOutButton'
 import { Icon, Icons } from '@/components/ui/Icons'
 import { authOptions } from '@/lib/auth'
@@ -34,27 +36,28 @@ const Layout = async ({ children }: LayoutProps) => {
     if (!session) {
         notFound()
     }
-    return <div className='w-full flex h-screen'>
-        <div className='flex h-full w-full max-w-xs grow flex-col gapy-5
-         overflow-y-auto border-r border-gray-200 bg-white px-6'>
-            <Link href="/dashboard" className='flex h-16 shrink-0 items-center'>
-                <Icons.Logo className='h-8 w-auto text-indigo-600' />
+    const unseenRequestCount = (await fetchRedis('smembers', `user:${session.user.id}:incoming_friend_requests`) as User[]).length
+
+    return <div className='flex w-full h-screen'>
+        <div className='flex flex-col w-full h-full max-w-xs px-6 overflow-y-auto bg-white border-r border-gray-200 grow gapy-5'>
+            <Link href="/dashboard" className='flex items-center h-16 shrink-0'>
+                <Icons.Logo className='w-auto h-8 text-indigo-600' />
             </Link>
             <div className="text-xs font-semibold leading-6 text-gray-400">Your Chats</div>
-            <nav className="flex flex-1 flex-col">
-                <ul role="list" className="flex flex-1 flex-col gap-y-7">
+            <nav className="flex flex-col flex-1">
+                <ul role="list" className="flex flex-col flex-1 gap-y-7">
                     <li>chat 1</li>
                     <li>
                         <div className='text-xs font-semibold leading-6 text-gray-600'>
                             Overview
                         </div>
-                        <ul role="list" className='-mx-2 mt-2 space-y-1'>
+                        <ul role="list" className='mt-2 -mx-2 space-y-1'>
                             {sidebaroptions.map((option) => {
                                 const Icon = Icons[option.Icon]
                                 return (
                                     <li key={option.id}>
-                                        <Link href={option.href} className="text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex gap-3 rounded-md p-2 text-sm leading-6 font-semibold">
-                                            <span className='text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white'>{<Icon className='h-4 w-4' />}</span>
+                                        <Link href={option.href} className="flex gap-3 p-2 text-sm font-semibold leading-6 text-gray-700 rounded-md hover:text-indigo-600 hover:bg-gray-50 group">
+                                            <span className='text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white'>{<Icon className='w-4 h-4' />}</span>
                                             <span className='truncate'>{option.name}</span>
                                         </Link>
                                     </li>
@@ -62,11 +65,13 @@ const Layout = async ({ children }: LayoutProps) => {
                             })}
                         </ul>
                     </li>
-                    <li className='-mx-6 flex mt-auto items-center'>
-                        <div className="flex flex-1 items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-gray-900 ">
-                            <div className='relative h-8 w-8 bg-gray-50'>
+                    <li>
+                        <FriendRequestSidebarOptions sessionId={session.user.id} initialUnseenRequestCount={unseenRequestCount} />
+                    </li>
+                    <li className='flex items-center mt-auto -mx-6'>
+                        <div className="flex items-center flex-1 px-6 py-3 text-sm font-semibold leading-6 text-gray-900 gap-x-4 ">
+                            <div className='relative w-8 h-8 bg-gray-50'>
                                 <Image fill referrerPolicy='no-referrer' className='rounded-full' src={session.user.image || ''} alt="Your Profile Picture" />
-
                             </div>
                             <span className='sr-only'>
                                 Your profile
