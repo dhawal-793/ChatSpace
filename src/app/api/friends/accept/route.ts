4b1f09b7e421556c60b79b62c47e623162d70446
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-06-20 17:19:18 +0530

    Updated accept route for friend request

--- src/app/api/friends/accept/route.ts
+++ src/app/api/friends/accept/route.ts
@@ -1,4 +1,5 @@
 import { authOptions } from "@/lib/auth"
+import { db } from "@/lib/db"
 import { getServerSession } from "next-auth"
 import { z } from "zod"
 import { fetchRedis } from "../add/helpers/redis"
@@ -6,17 +7,33 @@ import { fetchRedis } from "../add/helpers/redis"
 export async function POST(req: Request) {
     try {
         const body = await req.json()
-        const { id: idToAdd } = z.object({ id: z.string() }).parse(body)
+
         const session = await getServerSession(authOptions)
         if (!session) return new Response("Unauthorized", { status: 401 })
 
-        const isAlreadyFriend = await await fetchRedis('sismember', `user:${session.user.id}:friends`,idToAdd)
+        const { id: idToAdd } = z.object({ id: z.string() }).parse(body)
+
+        const isAlreadyFriend = await await fetchRedis('sismember', `user:${session.user.id}:friends`, idToAdd)
         if (isAlreadyFriend) return new Response("Already friends", { status: 400 })
 
         const hasFriendRequest = await fetchRedis('sismember', `user:${session.user.id}:incoming_friend_requests`, idToAdd)
-        console.log("hasFriendRequest =>, ", hasFriendRequest);
-        return new Response("ok")
-    } catch (error) {
+        if (!hasFriendRequest) {
+            return new Response("No friend request", { status: 400 })
+        }
+        await db.sadd(`user:${session.user.id}:friends`, idToAdd)
+        await db.sadd(`user:${idToAdd}:friends`, session.user.id)
 
+        // await db.srem(`user:${idToAdd}:incoming_friend_requests`, session.user.id)
+        await db.srem(`user:${session.user.id}:incoming_friend_requests`, idToAdd)
+
+
+        return new Response("OK", { status: 200 })
+    } catch (error) {
+        console.log(error);
+        if (error instanceof z.ZodError) {
+            return new Response('Invalid request payload', { status: 422 })
+        }
+
+        return new Response('Invalid request', { status: 400 })
     }
 }
