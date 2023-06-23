651a9cdb6191fbdb134e63882ede1802d690383f
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-06-23 19:05:18 +0530

    created types for Chat, Message and FriendRequest

--- src/types/db.d.ts
+++ src/types/db.d.ts
@@ -1,6 +1,25 @@
-interface User{
+interface User {
     name: string,
     email: string,
     image: string,
-    id:string
+    id: string
+}
+
+interface Chat {
+    id: string,
+    messages: Message[],
+}
+
+interface Message {
+    id: string,
+    senderId: string,
+    recieverId: string,
+    text: string,
+    timestamp: number
+}
+
+interface FreiendRequest {
+    id: string,
+    senderId: string,
+    recieverId: string,
 }
