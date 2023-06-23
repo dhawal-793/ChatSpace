5f2a615d9c6138c465af7abb3c4ad5f2d47d4f2f
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-06-23 19:09:33 +0530

    Added SidebarChatList and getFriendsByUserId inside layout

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -8,6 +8,8 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { notFound } from 'next/navigation'
 import { ReactNode } from 'react'
+import { getFriendsByUserId } from '@/helpers/get_Friends_By_UserId'
+import SideBarChatList from '@/components/SideBarChatList'
 
 interface LayoutProps {
     children: ReactNode
@@ -31,11 +33,13 @@ const sidebaroptions: SidebarOptions[] = [
 ]
 
 
+
 const Layout = async ({ children }: LayoutProps) => {
     const session = await getServerSession(authOptions)
     if (!session) {
         notFound()
     }
+    const friends = await getFriendsByUserId(session.user.id)
     const unseenRequestCount = (await fetchRedis('smembers', `user:${session.user.id}:incoming_friend_requests`) as User[]).length
 
     return <div className='flex w-full h-screen'>
@@ -43,20 +47,22 @@ const Layout = async ({ children }: LayoutProps) => {
             <Link href="/dashboard" className='flex items-center h-16 shrink-0'>
                 <Icons.Logo className='w-auto h-8 text-indigo-600' />
             </Link>
-            <div className="text-xs font-semibold leading-6 text-gray-400">Your Chats</div>
             <nav className="flex flex-col flex-1">
                 <ul role="list" className="flex flex-col flex-1 gap-y-7">
-                    <li>chat 1</li>
+                    {friends.length > 0 && <li>
+                        <div className="text-xs font-semibold leading-6 text-gray-400">Your Chats</div>
+                        <SideBarChatList friends={friends} />
+                    </li>}
                     <li>
                         <div className='text-xs font-semibold leading-6 text-gray-600'>
                             Overview
                         </div>
-                        <ul role="list" className='mt-2 -mx-2 space-y-1'>
+                        <ul role="list" className='mt-2 space-y-1'>
                             {sidebaroptions.map((option) => {
                                 const Icon = Icons[option.Icon]
                                 return (
                                     <li key={option.id}>
-                                        <Link href={option.href} className="flex gap-3 p-2 text-sm font-semibold leading-6 text-gray-700 rounded-md hover:text-indigo-600 hover:bg-gray-50 group">
+                                        <Link href={option.href} className="flex gap-3 p-2 pl-0 text-sm font-semibold leading-6 text-gray-700 rounded-md hover:text-indigo-600 hover:bg-gray-50 group">
                                             <span className='text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white'>{<Icon className='w-4 h-4' />}</span>
                                             <span className='truncate'>{option.name}</span>
                                         </Link>
