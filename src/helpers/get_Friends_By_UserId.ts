ec17a2a38140c42f58261ea8cc77096164f8c6bd
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-06-25 10:03:02 +0530

    Updated getFriendsByUserId method

--- src/helpers/get_Friends_By_UserId.ts
+++ src/helpers/get_Friends_By_UserId.ts
@@ -4,8 +4,9 @@ export const getFriendsByUserId = async (userId: string) => {
     const friendIds = await fetchRedis('smembers', `user:${userId}:friends`) as string[];
 
     const friends = await Promise.all(friendIds.map(async (friendId) => {
-        const friend = await fetchRedis('get', `user:${friendId}`) as User;
-        return friend;
+        const friend = await fetchRedis('get', `user:${friendId}`) as string;
+        const parsedFriend = JSON.parse(friend) as User;
+        return parsedFriend;
     }))
 
     return friends;
