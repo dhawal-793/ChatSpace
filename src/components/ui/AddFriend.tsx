a89016a9ae0a1d4779e3d80b4385a7f9dbadebda
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-06-27 21:12:58 +0530

    Update success state, whenever function is called.

--- src/components/ui/AddFriend.tsx
+++ src/components/ui/AddFriend.tsx
@@ -41,12 +41,13 @@ const AddFriend: FC<AddFriendProps> = ({ }) => {
     }
 
     const onSubmit = (data: FormData) => {
+        setShowSuccessState(false)
         addFriend(data.email)
     }
 
     return <form className='max-w-sm' onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="email" className='block text-sm font-medium leading-6 text-gray-900'>Add Friend by E-Mail</label>
-        <div className="mt-2 flex gap-4 ">
+        <div className="flex gap-4 mt-2 ">
             <input {...register('email')}
                 type="text" className='block w-full rounded-md border-0  py-1.5 text-gray-900 shadow-sm ring-1 ring-inset
             ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm leading-6' placeholder='you@example.com' />
