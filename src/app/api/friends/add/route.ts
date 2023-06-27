587cb9bbd04c340fb54179052b009c2b400893fb
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-06-27 21:04:44 +0530

    Bug fixed while checking isAlreadyAdded

--- src/app/api/friends/add/route.ts
+++ src/app/api/friends/add/route.ts
@@ -25,16 +25,14 @@ export async function POST(req: Request) {
             return new Response('You cannot add yourself as a friend', { status: 400 })
         }
 
-        const isAlreadyAdded = await (fetchRedis('sismember', `user:${idToAdd}:incomming_friend_requests`, session.user.id)) as 0 | 1
+        const isAlreadyAdded = await (fetchRedis('sismember', `user:${idToAdd}:incoming_friend_requests`, session.user.id)) as 0 | 1
         if (isAlreadyAdded) {
-            return new Response('You already added this user', { status: 400 })
-
+            return new Response('You already sent friend request this user', { status: 400 })
         }
 
         const isAlreadyfriends = await (fetchRedis('sismember', `user:${session.user.id}:friends`, idToAdd)) as 0 | 1
         if (isAlreadyfriends) {
             return new Response('You are already friend of this user', { status: 400 })
-
         }
 
         pusherServer.trigger(toPusherKey(`user:${idToAdd}:incoming_friend_requests`), 'incoming_friend_requests', {
