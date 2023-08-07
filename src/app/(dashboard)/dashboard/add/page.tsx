45fba4abe1d08108fd983966a3b2c57c4eee8874
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-08-07 22:15:05 +0530

    Updated styling.

--- src/app/(dashboard)/dashboard/add/page.tsx
+++ src/app/(dashboard)/dashboard/add/page.tsx
@@ -3,7 +3,7 @@ import AddFriend from "@/components/ui/AddFriend"
 
 const Page = () => {
     return (
-        <main className="pt-8">
+        <main className="w-full">
             <h1 className="mb-8 text-5xl font-bold ">Add a Friend</h1>
             <AddFriend />
         </main>
